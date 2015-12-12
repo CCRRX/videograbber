91e8a5cd7b70f2021f5c8f79643126bdc20230c4
Marcel <MTRNord@users.noreply.github.com>
2015-12-13 00:39:19 +0100

    Update Rechtschreibung

--- videograbber.sh
+++ videograbber.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
  
 echo "by CRX"
-for i in `seq 1 20928`; #20928 is the maximum nuber of al video sites ... i thin today i will be some more!
+for i in `seq 1 20928`; #20928 is the maximum number of all video pages ... i think today there will be some more!
         do
 echo -e "CRX IS DA FUCKIN \033[31mPoRnO\033[0m. KING"
 wget -O - http://de.xhamster.com/new/$i.html |   grep -o '<a href=['"'"'"][^"'"'"']*['"'"'"]' | grep -o '<a href=['"'"'"][^"'"'"']*['"'"'"]' | sed -e 's/^<a href=["'"'"']//' -e 's/["'"'"']$//' | grep http://de.xhamster.com/movies/ >> listeallmain.txt
